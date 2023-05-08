e85762288cb327ba863c9f3f6b8fda29d4b48864
Anthony Loria <agloria2001@gmail.com>
2023-05-08 11:34:32 -0600

    Created main and lexer files and added header to the main file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// MyLexer
+//
+// MyLexer is a custom Lexical Analyzer Generator that allows the user to define a subset of rules in which the
+// program would pair those rules with a token and generate a Lexical Analyzer for those specified rules to be used
+// in another project. 
+//
+//
+//  Author: Anthony Loria
+//
+//  License: N/A (At the moment)
+
